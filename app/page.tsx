8d7a260699889a1d82e45ee7dc75ec4cf5261f9f
Adnan Salim <adnanzabdi@outlook.com>
2025-05-28 16:41:49 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 const page = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js 👋</h1>
+      <h1 className="h1-bold">Welcome to Next.js</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;

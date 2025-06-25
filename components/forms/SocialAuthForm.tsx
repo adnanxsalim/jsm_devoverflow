6b6b3793f726ec9cf12500a7b801fc8e61477bfa
Adnan Salim <adnanzabdi@outlook.com>
2025-06-25 14:20:54 +0530

    update social-auth-form

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -11,13 +11,12 @@ import { Button } from "../ui/button";
 
 const SocialAuthForm = () => {
   const buttonClass =
-    "background-dark400_light900 body-medium text-dark200_light800 rounded-2 min-h-12 flex-1 px-4 py-3.5";
+    "background-dark400_light900 body-medium text-dark200_light800 rounded-2 min-h-12 flex-1 px-4 py-3.5 cursor-pointer";
 
   const handleSignIn = async (provider: "github" | "google") => {
     try {
       await signIn(provider, {
         callbackUrl: ROUTES.HOME,
-        // redirect: false,
       });
     } catch (error) {
       console.log(error);

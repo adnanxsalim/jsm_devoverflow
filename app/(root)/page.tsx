65712a34bd0b699a86b3610c874320eb71c45e35
Adnan Salim <adnanzabdi@outlook.com>
2025-06-25 14:21:07 +0530

    add log out button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
-const page = () => {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
+
   return (
     <div>
       <h1 className="h1-bold">Welcome to Next.js</h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit" className="cursor-pointer bg-white text-black">
+          Log out
+        </Button>
+      </form>
     </div>
   );
 };
 
-export default page;
+export default Home;
